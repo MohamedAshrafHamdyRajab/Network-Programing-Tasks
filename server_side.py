ca79fe6368e6f91fc82c052b678121849621c9a8
MohamedAshrafHamdyRajab <120051603+MohamedAshrafHamdyRajab@users.noreply.github.com>
2024-03-08 20:30:53 +0300

    Update server_side.py

--- server_side.py
+++ server_side.py
@@ -4,11 +4,11 @@ print ("The socket created")
 host ='127.0.0.1'
 port = 40444
 s.bind((host, port)) 
-print ("The socket binded to ",port) 
+print("The socket binded to ",port) 
 s.listen(1)
 print ("The socket is listening") 
 while True:
-    connection , address= s.accept() 
+    connection , address = s.accept() 
     print ('The server get connection from address',address)
     connection.send(b'Thank you for connecting with the server')
     connection.close()
